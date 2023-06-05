29d335facee28af24ffab3cdd3de3a4a9ff4673d
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-06-05 16:06:23 +0100

    thrid step

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.css']
+  styleUrls: ['./cv.component.css'],
 })
 export class CvComponent {
-
+  tabCandidats: Candidat[] = [
+    new Candidat(1, 'bart', 'simpson', 23, 'ingénieur', 'bart.jpeg'),
+    new Candidat(2, 'homer', 'simpson', 52, 'directeur', 'homer.jpg'),
+    new Candidat(3, 'lisa', 'simpson', 18, 'etudiante', 'lisa.png'),
+  ];
 }
