ca629b350e5c1d5ee4385a0846cc45693f090d09
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-06-05 15:31:59 +0100

    first step project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'DataScientest-June-23';
+
+  traiterEvent(msg: string) {
+    this.title = msg;
+  }
 }
