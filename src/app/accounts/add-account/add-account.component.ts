4de53c51a9b57726d80251303bfb8134705829e0
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-06-06 11:17:01 +0100

    final step accounts

--- src/app/accounts/add-account/add-account.component.ts
+++ src/app/accounts/add-account/add-account.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-account',
   templateUrl: './add-account.component.html',
-  styleUrls: ['./add-account.component.css']
+  styleUrls: ['./add-account.component.css'],
 })
 export class AddAccountComponent {
+  newName: string;
+  newStatut: string;
+  @Output() sendAccountToHome = new EventEmitter();
 
+  sendToHome() {
+    this.sendAccountToHome.emit({
+      name: this.newName,
+      statut: this.newStatut,
+    });
+  }
 }
--- src/app/accounts/home-account/home-account.component.ts
+++ src/app/accounts/home-account/home-account.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-account',
   templateUrl: './home-account.component.html',
-  styleUrls: ['./home-account.component.css']
+  styleUrls: ['./home-account.component.css'],
 })
 export class HomeAccountComponent {
+  tabAccounts = [
+    {
+      name: 'Nidhal Account',
+      statut: 'active',
+    },
+    {
+      name: 'Peter Account',
+      statut: 'inactive',
+    },
+  ];
 
+  addNewAccount(newAcc) {
+    this.tabAccounts.push(newAcc);
+    console.log(this.tabAccounts);
+  }
 }
