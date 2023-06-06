0224207a1fa220c062cfbb95a20fddb9e074ce39
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-06-06 14:15:36 +0100

    add manage-servers

---
+++ src/app/manage-servers/manage-servers.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-servers',
+  templateUrl: './manage-servers.component.html',
+  styleUrls: ['./manage-servers.component.css'],
+})
+export class ManageServersComponent {
+  listServers = [
+    {
+      nom: 'Production server',
+      type: 'small',
+      date_d: new Date(2, 4, 2020),
+      statut: 'critical',
+    },
+    {
+      nom: 'Testing Development server',
+      type: 'large',
+      date_d: new Date(2, 4, 2020),
+      statut: 'stable',
+    },
+    {
+      nom: 'Development server',
+      type: 'medium',
+      date_d: new Date(2, 4, 2020),
+      statut: 'offline',
+    },
+    {
+      nom: 'Nidhal server',
+      type: 'small',
+      date_d: new Date(2, 4, 2020),
+      statut: 'stable',
+    },
+  ];
+
+  affecterClasse(st) {
+    return {
+      //'list-group-item-success' : st == 'active' ? true : false,
+      'list-group-item-success': st == 'stable',
+      'list-group-item-danger': st == 'critical',
+      'list-group-item-warning': st == 'offline',
+    };
+  }
+}
