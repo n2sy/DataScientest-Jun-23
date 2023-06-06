ae1f073f3a72ab4b8adf440e4903ce543285e224
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-06-06 10:08:48 +0100

    first step accounts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,12 @@ import { CvComponent } from './cv/cv.component';
 import { ListeComponent } from './liste/liste.component';
 import { DetailsComponent } from './details/details.component';
 import { ItemComponent } from './item/item.component';
+import { AddAccountComponent } from './accounts/add-account/add-account.component';
+import { HomeAccountComponent } from './accounts/home-account/home-account.component';
+import { ListAccountComponent } from './accounts/list-account/list-account.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, SecondComponent, CvComponent, ListeComponent, DetailsComponent, ItemComponent],
+  declarations: [AppComponent, FirstComponent, SecondComponent, CvComponent, ListeComponent, DetailsComponent, ItemComponent, AddAccountComponent, HomeAccountComponent, ListAccountComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
