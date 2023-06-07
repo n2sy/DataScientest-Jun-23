76a46c042cf67ba67a1b1acd5aa21bf80a024c11
nidhal <nidhal.jelassi@fsegt.utm.tn>
2023-06-07 15:52:38 +0100

    add submodule

---
+++ src/app/sub/sub.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeServersComponent } from './home-servers/home-servers.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeServersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SubModule { }
